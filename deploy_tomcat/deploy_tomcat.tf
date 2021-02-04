3a6cf28b88a9f0355b5edbfb069059af8c057b8f
vbkr1598 <68968706+vbkr1598@users.noreply.github.com>
2021-02-04 16:27:29 +0530

    Update deploy_tomcat.tf

--- deploy_tomcat/deploy_tomcat.tf
+++ deploy_tomcat/deploy_tomcat.tf
@@ -1,6 +1,7 @@
 resource "null_resource" "copy_to_tom" {
   provisioner "local-exec" {
-    command = "echo 'T9s4krwJb336' | sudo -S cp /var/lib/jenkins/workspace/vib_assign/target/spring-mvc-example.war /opt/tomcat/tomcat9/webapps/spring-mvc-example.war"
+    //command = "echo 'T9s4krwJb336' | sudo -S cp /var/lib/jenkins/workspace/vib_assign/target/spring-mvc-example.war /opt/tomcat/tomcat9/webapps/spring-mvc-example.war"
+    command = "sudo cp /var/lib/jenkins/workspace/vib_assign/target/spring-mvc-example.war /opt/tomcat/tomcat9/webapps/spring-mvc-example.war"
     //interpreter = ["PowerShell", "-Command"]
   }
 }
